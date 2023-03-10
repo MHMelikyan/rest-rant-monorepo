ea568e6bf3e39f75c3f243ca5b5de511289a5c02
Melik Melikyan <mhmelikyan@gmail.com>
2023-03-09 20:42:08 -0500

    useEffect added

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -10,7 +10,7 @@ function CurrentUserProvider({ children }){
     useEffect(() => {
 
         const getLoggedInUser = async () => {
-            let response = await fetch('http://localhost:5000/authentication/profile')
+            let response = await fetch('http://localhost:3123/authentication/profile')
             let user = await response.json()
             setCurrentUser(user)
         }
